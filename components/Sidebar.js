2d1e5377b33d6bc56735b4a5bc590dbc4f1bc8a5
eightsuzuki <suzukieight8@gmail.com>
2024-01-26 00:35:17 +0900

    Create the Skelton of Sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from "next/image";
+
+export default function Sidebar() {
+  return (
+    <div>
+        <Image
+          width="50"
+          height="50"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+        ></Image>
+    </div>
+  )
+}
