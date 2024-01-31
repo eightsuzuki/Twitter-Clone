c4d4c84dc267134277965153720e85188e7c4f4e
eightsuzuki <suzukieight8@gmail.com>
2024-02-01 01:23:48 +0900

    Align the CSS of Post

--- components/Feed.js
+++ components/Feed.js
@@ -6,8 +6,8 @@ export default function Feed() {
   const posts = [
     {
       id: "1",
-      name: "Sahand Ghavidel",
-      username: "codewithsahand",
+      name: "sugiki8",
+      username: "sugiki8",
       userImg: "https://eightsuzuki.github.io/images/profile.png",
       img: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80",
       text: "nice view!",
@@ -15,8 +15,8 @@ export default function Feed() {
     },
     {
       id: "2",
-      name: "Sahand Ghavidel",
-      username: "codewithsahand",
+      name: "sugiki8",
+      username: "sugiki8",
       userImg: "https://eightsuzuki.github.io/images/profile.png",
       img: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
       text: "wow!",
