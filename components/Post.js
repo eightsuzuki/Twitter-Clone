9f2dcff336a789f70bc37e0ac55c0e6d18cf9358
eightsuzuki <suzukieight8@gmail.com>
2024-02-01 01:15:00 +0900

    Add the Contents of Post

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,61 @@
+import {
+    ChartBarIcon,
+    ChatIcon,
+    DotsHorizontalIcon,
+    HeartIcon,
+    ShareIcon,
+    TrashIcon,
+  } from "@heroicons/react/outline";
+
 export default function Post( {post} ) {
   return (
-    <div>{post.id + post.text}</div>
+    <div className="">
+      {/* user image */}
+      <img
+        className=""
+        src={post.userImg}
+        alt="user-img"
+      />
+      {/* right side */}
+      <div className="">
+        {/* Header */}
+
+        <div className="">
+          {/* post user info */}
+          <div className="">
+            <h4 className="">
+              {post.name}
+            </h4>
+            <span className="">@{post.username} - </span>
+            <span className="">
+              {post.timestamp}
+            </span>
+          </div>
+
+          {/* dot icon */}
+          <DotsHorizontalIcon className="" />
+        </div>
+
+        {/* post text */}
+
+        <p className="">
+          {post.text}
+        </p>
+
+        {/* post image */}
+
+        <img className="" src={post.img} alt="" />
+
+        {/* icons */}
+
+        <div className="">
+          <ChatIcon className="" />
+          <TrashIcon className="" />
+          <HeartIcon className="" />
+          <ShareIcon className="" />
+          <ChartBarIcon className="" />
+        </div>
+      </div>
+    </div>
   )
 }
