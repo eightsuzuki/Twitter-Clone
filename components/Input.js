d018fbd73232fc4d4b13c18e004f13ed79ba8a93
eightsuzuki <suzukieight8@gmail.com>
2024-02-03 00:51:57 +0900

    Create the News Component of Widgets

--- components/Input.js
+++ components/Input.js
@@ -10,14 +10,20 @@ export default function Input() {
       />
       <div className="w-full divide-y divide-gray-200">
         <div className="">
-          <textarea className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700" rows="2" placeholder="What's happening?"></textarea>
+          <textarea
+            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+            rows="2"
+            placeholder="What's happening?"
+          ></textarea>
         </div>
         <div className="flex items-center justify-between pt-2.5">
-            <div className="flex">
-                <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
-                <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100"/>
-            </div>
-            <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">Tweet</button>
+          <div className="flex">
+            <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+            <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+          </div>
+          <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
+            Tweet
+          </button>
         </div>
       </div>
     </div>
