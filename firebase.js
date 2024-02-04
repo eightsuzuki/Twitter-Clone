8d105c6ea63a1231ac4e44dbb228eeb4e57af525
eightsuzuki <suzukieight8@gmail.com>
2024-02-04 23:53:49 +0900

    Add Contents of Signin

--- firebase.js
+++ firebase.js
@@ -6,7 +6,6 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-v4-e528c.firebaseapp.com",
