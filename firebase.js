fa952248fba43df891f154a6f57f3ff313a1bc4b
eightsuzuki <suzukieight8@gmail.com>
2024-02-04 23:23:45 +0900

    Create the Skelton of Signin

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-v4-e528c.firebaseapp.com",
   projectId: "twitter-v4-e528c",
   storageBucket: "twitter-v4-e528c.appspot.com",
