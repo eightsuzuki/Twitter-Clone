644ce3f936d236ffe21fbf7699e896a35f796b8a
eightsuzuki <suzukieight8@gmail.com>
2024-02-05 23:35:53 +0900

    Install and Implement React-Modal

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import CommentModal from "../components/CommentModal";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
@@ -28,6 +29,8 @@ export default function Home({ newsResults, randomUsersResults }) {
         />
 
         {/* Modal */}
+
+        <CommentModal />
       </main>
     </div>
   );
@@ -40,11 +43,12 @@ export async function getServerSideProps() {
     "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
   ).then((res) => res.json());
 
-    // Who to follow section
+  // Who to follow section
 
-    const randomUsersResults = await fetch(
-      "https://randomuser.me/api/?results=30&inc=name,login,picture"
-    ).then((res) => res.json());
+  const randomUsersResults = await fetch(
+    "https://randomuser.me/api/?results=30&inc=name,login,picture"
+  ).then((res) => res.json());
+
   return {
     props: {
       newsResults,
