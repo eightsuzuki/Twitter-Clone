3455a82f5520f4b4467d1fead6053f6e554839c1
eightsuzuki <suzukieight8@gmail.com>
2024-04-14 15:01:30 +0900

    Adapte guidelines for Google account login buttons

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -3,27 +3,41 @@ import { getProviders, signIn } from "next-auth/react";
 export default function signin({ providers }) {
   return (
     <div className="flex justify-center mt-20 space-x-4">
-      <img
-        src="https://cdn.cms-twdigitalassets.com/content/dam/help-twitter/en/twitter-tips/desktop-assets/ch-01/ch12findphone.png.twimg.1920.png"
-        alt="twitter image inside a phone"
-        className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
-      />
       <div className="">
         {Object.values(providers).map((provider) => (
           <div key={provider.name} className="flex flex-col items-center">
-            <img
-              className="w-36 object-cover"
-              src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-              alt="twitter logo"
-            />
-            <p className="text-center text-sm italic my-10">
-              This app is created for learning purposes
-            </p>
             <button
               onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+              class="gsi-material-button bg-white border border-gray-600 rounded-md font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-700 active:bg-opacity-25 disabled:bg-gray-100 disabled:border-gray-200 disabled:text-gray-500 disabled:opacity-50 flex items-center justify-between px-4 h-10"
             >
-              Sign in with {provider.name}
+              <div class="gsi-material-button-icon flex-shrink-0 h-5 w-5 mr-2">
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#EA4335"
+                    d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+                  ></path>
+                  <path
+                    fill="#4285F4"
+                    d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+                  ></path>
+                  <path
+                    fill="#FBBC05"
+                    d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+                  ></path>
+                  <path
+                    fill="#34A853"
+                    d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+                  ></path>
+                  <path fill="none" d="M0 0h48v48H0z"></path>
+                </svg>
+              </div>
+              <span class="gsi-material-button-contents flex-grow overflow-hidden">
+                Sign in with Google
+              </span>
             </button>
           </div>
         ))}
