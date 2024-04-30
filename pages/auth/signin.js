53a7696c962924bc6b1dd374e60c8afa74d39728
eightsuzuki <suzukieight8@gmail.com>
2024-04-30 16:52:21 +0900

    Update Auth System to Firebase and Remove Next-Auth

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,56 +1,69 @@
-import { getProviders, signIn } from "next-auth/react";
-
-export default function signin({ providers }) {
+import { db } from "../../firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+export default function Signin() {
+  const router = useRouter();
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+      const user = auth.currentUser.providerData[0];
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          username: user.displayName.split(" ").join("").toLocaleLowerCase(),
+          userImg: user.photoURL,
+          uid: user.uid,
+          timestamp: serverTimestamp(),
+        });
+      }
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex justify-center mt-20 space-x-4">
       <div className="">
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name} className="flex flex-col items-center">
-            <button
-              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-              className="gsi-material-button bg-white border border-gray-600 rounded-md font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-700 active:bg-opacity-25 disabled:bg-gray-100 disabled:border-gray-200 disabled:text-gray-500 disabled:opacity-50 flex items-center justify-between px-4 h-10"
+        <button
+          onClick={onGoogleClick}
+          className="gsi-material-button bg-white border border-gray-600 rounded-md font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-700 active:bg-opacity-25 disabled:bg-gray-100 disabled:border-gray-200 disabled:text-gray-500 disabled:opacity-50 flex items-center justify-between px-4 h-10"
+        >
+          <div className="gsi-material-button-icon flex-shrink-0 h-5 w-5 mr-2">
+            <svg
+              className="fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 48 48"
             >
-              <div className="gsi-material-button-icon flex-shrink-0 h-5 w-5 mr-2">
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 48 48"
-                >
-                  <path
-                    fill="#EA4335"
-                    d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
-                  ></path>
-                  <path
-                    fill="#4285F4"
-                    d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
-                  ></path>
-                  <path
-                    fill="#FBBC05"
-                    d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
-                  ></path>
-                  <path
-                    fill="#34A853"
-                    d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
-                  ></path>
-                  <path fill="none" d="M0 0h48v48H0z"></path>
-                </svg>
-              </div>
-              <span className="gsi-material-button-contents flex-grow overflow-hidden">
-                Sign in with Google
-              </span>
-            </button>
+              <path
+                fill="#EA4335"
+                d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+              ></path>
+              <path
+                fill="#4285F4"
+                d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+              ></path>
+              <path
+                fill="#FBBC05"
+                d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+              ></path>
+              <path
+                fill="#34A853"
+                d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+              ></path>
+              <path fill="none" d="M0 0h48v48H0z"></path>
+            </svg>
           </div>
-        ))}
+          <span className="gsi-material-button-contents flex-grow overflow-hidden">
+            Sign in with Google
+          </span>
+        </button>
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const providers = await getProviders();
-  return {
-    props: {
-      providers,
-    },
-  };
 }
